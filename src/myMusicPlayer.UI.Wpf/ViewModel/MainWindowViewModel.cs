be96867e57f0359aaea7eabe362a4403a4bfd9e7
RedesajnDesktop\Peter Centellini <peter.centellini@redesajn.com>
2023-03-21 20:32:44 +0100

    Added the Core infrastructure. Added images. Added the UserControls and MainWindow. Moved MainWindow to the View folder. Added ViewModel for MainWindow. Implemented the top button commands. Added styling.

---
+++ src/myMusicPlayer.UI.Wpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using myMusicPlayer.UI.Wpf.Core;
+using System.Windows;
+
+namespace myMusicPlayer.UI.Wpf.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public RelayCommand MoveWindowCommand { get; set; }
+        public RelayCommand ShutdownWindowCommand { get; set; }
+        public RelayCommand MinimizeWindowCommand { get; set; }
+        public RelayCommand MaximizeWindowCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+            ShutdownWindowCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
+            MinimizeWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.WindowState = WindowState.Minimized; });
+
+            MaximizeWindowCommand = new RelayCommand(o =>
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                }
+            });
+        }
+    }
+}
