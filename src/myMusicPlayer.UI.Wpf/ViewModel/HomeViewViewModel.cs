61c54d0a93cf40a1194b95e7996587747964ebb4
RedesajnDesktop\Peter Centellini <peter.centellini@redesajn.com>
2023-03-23 18:09:54 +0100

    Added the HomeView with content and its ViewModel. Made changes to the MainWindow to suit the HomeView. Added to App.Xaml.

---
+++ src/myMusicPlayer.UI.Wpf/ViewModel/HomeViewViewModel.cs
@@ -0,0 +1,13 @@
+﻿using myMusicPlayer.UI.Wpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myMusicPlayer.UI.Wpf.ViewModel
+{
+    public class HomeViewViewModel : ViewModelBase
+    {
+    }
+}
--- src/myMusicPlayer.UI.Wpf/ViewModel/MainWindowViewModel.cs
+++ src/myMusicPlayer.UI.Wpf/ViewModel/MainWindowViewModel.cs
@@ -5,13 +5,38 @@ namespace myMusicPlayer.UI.Wpf.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        #region Commands
+
         public RelayCommand MoveWindowCommand { get; set; }
         public RelayCommand ShutdownWindowCommand { get; set; }
         public RelayCommand MinimizeWindowCommand { get; set; }
         public RelayCommand MaximizeWindowCommand { get; set; }
 
+        #endregion
+
+        #region Properties
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { SetProperty(ref _currentView, value); }
+        }
+
+        #endregion
+
+        #region ViewModels
+
+        public HomeViewViewModel HomeViewVM { get; set; }
+
+        #endregion
+
         public MainWindowViewModel()
         {
+            HomeViewVM = new HomeViewViewModel();
+
+            _currentView = HomeViewVM;
+
             MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
             ShutdownWindowCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
             MinimizeWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.WindowState = WindowState.Minimized; });
